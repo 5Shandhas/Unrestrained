4345e63f02c83825d8fea31b6d6242b5efb2311d
5Skandhas <yang6261477@sina.com>
2019-03-17 22:59:26 +0800

    year's start update

--- mainapp.py
+++ mainapp.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config.update(DEBUG=True)
+app.config['SECRET_KEY'] = '15615156dhiuahdkbafiuwahfw'
 
 posts = [
 	{
@@ -17,6 +21,16 @@ def mainapp():
 @app.route("/about")
 def about():
 	return "<h2>About Pages<h2>"
+
+@app.route("/register")
+def register():
+	form = RegistrationForm()
+	return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+	form = LoginForm()
+	return render_template('login.html', title='login', form=form)
 
 if __name__ == '__main__':
 	app.run(debug=True)
